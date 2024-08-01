520202b605a6a266521c72543581f2703ae6f949
Georgy <bachakashvili.ga@gmail.com>
2024-08-01 15:45:08 +0300

    codecrafters submit [skip ci]

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -38,7 +38,7 @@ func main() {
 }
 
 func matchLine(line []byte, pattern string) (bool, error) {
-	if utf8.RuneCountInString(pattern) != 1 {
+	if utf8.RuneCountInString(pattern) == 0 {
 		return false, fmt.Errorf("unsupported pattern: %q", pattern)
 	}
 
@@ -47,8 +47,11 @@ func matchLine(line []byte, pattern string) (bool, error) {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this to pass the first stage
-	ok = bytes.ContainsAny(line, pattern)
+    if pattern == "\\d" {
+        ok = bytes.ContainsAny(line, "0123456789")
+    } else {
+        ok = bytes.ContainsAny(line, pattern)
+    }
 
 	return ok, nil
 }
