38825803c1743603459274aa7c7cad1d2c97b0ec
Georgy <bachakashvili.ga@gmail.com>
2024-08-05 21:00:14 +0300

    codecrafters submit [skip ci]

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	// Uncomment this to pass the first stage
-	"bytes"
 	"fmt"
 	"io"
 	"os"
+	"strings"
 	"unicode/utf8"
 )
 
@@ -37,11 +37,89 @@ func main() {
 	// default exit code is 0 which means success
 }
 
-func matchWord(letter rune) bool {
-    if !(letter >= 'A' && letter <='Z' || letter >= 'a' && letter <= 'z' || letter >= '0' && letter <= '9' || letter == '_') {
-        return false
+
+func isWord(letter byte) bool {
+    return isDigit(letter) || isLetter(letter) || letter == '_'
+}
+
+func isDigit(letter byte) bool {
+    return letter >= '0' && letter <= '9'
+}
+
+func isLetter(letter byte) bool {
+    return letter >= 'A' && letter <='Z' || letter >= 'a' && letter <= 'z'
+}
+
+func isSpecial(letter byte) bool {
+    return strings.Contains("\\[]()^$", string(letter))
+}
+
+func matchHere(line string, pattern string, here int) bool {
+    p := 0
+
+    for l := here; l < len(line); {
+        fmt.Printf("Line pointer now points at %v\n", l)
+        fmt.Printf("Pattern pointer now points at %v\n", p)
+        if pattern[p] == '\\' {
+            if p + 1 >= len(pattern) {
+                // panic("pattern ended unexpectedly")
+            }
+            p += 1
+            if pattern[p] == 'd' {
+                // println("The pattern is \\d")
+                if !isDigit(line[l]) {
+                    return false
+                } else {
+                    p += 1
+                }
+                l += 1
+            } else if pattern[p] == 'w' {
+                // println("The pattern is \\w")
+                if !isWord(line[l]) {
+                    return false
+                } else {
+                    p += 1
+                }
+                l += 1
+            }
+        } else if pattern[p] == '[' {
+            closing := strings.Index(pattern[p:], string(']'))
+            if closing == -1 {
+                panic("no closing bracket")
+            }
+            // println(closing)
+            // println(p+1, closing+p+1)
+            group := pattern[p+1:closing+p]
+            println(group)
+            if group[0] == '^' {
+                if strings.Contains(group[1:], string(line[l])) {
+                    return false
+                } else {
+                    p += closing + 2
+                }
+                l += 1
+            } else {
+                if strings.Contains(group, string(line[l])) {
+                    p += closing + 2
+                } else {
+                    return false
+                }
+                l += 1
+            }
+        } else {
+            // fmt.Printf("No pattern, just word")
+            if line[l] == pattern[p] {
+                p += 1
+                l += 1
+            } else {
+                return false
+            }
+        }
+        if p >= len(pattern) {
+            return true
+        }
     }
-    return true
+    return false
 }
 
 func matchLine(line []byte, pattern string) (bool, error) {
@@ -49,24 +127,15 @@ func matchLine(line []byte, pattern string) (bool, error) {
 		return false, fmt.Errorf("unsupported pattern: %q", pattern)
 	}
 
-	var ok bool
 
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-    if pattern == "\\d" {
-        ok = bytes.ContainsAny(line, "0123456789")
-    } else if pattern == "\\w" {
-        ok = bytes.ContainsFunc(line, matchWord)
-    } else if pattern[0] == '[' && pattern[len(pattern)-1] == ']' {
-        ok = bytes.ContainsAny(line, pattern[1:len(pattern)-1])
-        if len(pattern) >= 3 && pattern[1] == '^' {
-            ok = !ok
-        } 
-    } else {
-        ok = bytes.ContainsAny(line, pattern)
-    } 
-    println(ok)
-
-	return ok, nil
+    for i := range line {
+        if matchHere(string(line), pattern, i) {
+            print(true)
+            return true, nil
+        }
+    }
+    return false, nil
 }
